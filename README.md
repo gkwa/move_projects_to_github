d0ef8a57c638f6b5081fa221ecd322adb66e0b60
taylormonacelli <tailor@u.washington.edu>
2014-05-26 13:41:29 -0700

    yada-yada

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Tutorials / tidbits
 ------------------------------------
 * http://blog.troygrosfield.com/2010/12/18/parsing-xml-with-python-using-elementtree
 * finall() https://docs.python.org/2/library/xml.etree.elementtree.html
+* python add elements to a set http://stackoverflow.com/questions/3392354/python-append-values-to-a-set
 * 
--- t.py
+++ t.py
@@ -1,9 +1,24 @@
 # -*- python -*-
 
 import xml.etree.cElementTree as ET
-tree = ET.ElementTree(file='/Users/demo/pdev/manifest/default.xml')
+pdevtree = ET.ElementTree(file='/Users/demo/pdev/manifest/default.xml')
+pdghtree = ET.ElementTree(file='/Users/demo/pdgh/manifest/default.xml')
 
-for elem in tree.iter('project'):
+from sets import Set
+
+pdghset = set()
+pdevset = set()
+
+for elem in pdevtree.iter('project'):
     if 'name' in elem.attrib:
-        print elem.attrib['name']
+        pdevset.add(elem.attrib['name'])
 
+for elem in pdghtree.iter('project'):
+    if 'name' in elem.attrib:
+        pdghset.add(elem.attrib['name'])
+
+for project in pdghset.intersection(pdevset):
+    print "already merged: %s" % project
+
+for project in pdevset.difference(pdghset):
+    print "need to merge: %s" % project
