df778db9602be981d5126f8943cf17d24f2d59d5
taylormonacelli <tailor@u.washington.edu>
2014-05-26 19:39:28 -0700

    yada-yada

--- templates/move_template.sh
+++ templates/move_template.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pushd ~/pdgh/manifest && python ~/pdev/python-practice/add-remove-xml-nodes/t.py -f ~/pdgh/manifest/default.xml --add "{{ project }}" && git commit -m "Add {{ project }} project" -a && git show --color
 git push && popd && git remote add gh git@github.com:taylormonacelli/{{ project }}.git
-git push --set-upstream gh master --force && git push gh --all
+git push --set-upstream gh master --force; git push gh --all
 python ~/pdev/python-practice/add-remove-xml-nodes/t.py -f ~/pdev/manifest/default.xml --remove "{{ project }}" && cd ~/pdev/manifest && git diff --color && git commit -m "Move {{ project }} project to github" -a
 git push
 ssh dev
