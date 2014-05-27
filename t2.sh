b1fb3d131c5a71c05c04afb388621b5c4422c55e
taylormonacelli <tailor@u.washington.edu>
2014-05-26 19:03:57 -0700

    yada-yada

--- t2.sh
+++ t2.sh
@@ -3,4 +3,3 @@
 proj=$(python ~/pdev/python-practice/add-remove-xml-nodes/t.py -f ~/pdev/manifest/default.xml --rand)
 echo "export proj=$proj && cd ~/pdev/$proj && git co -t dev/master ||:"
 echo python ~/pdgh/move_projects_to_github/create_move_script_from_template.py -p $proj
-echo cat move.sh
