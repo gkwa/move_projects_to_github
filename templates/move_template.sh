063a8d69741771231251f7c453784c4cf2c9a027
taylormonacelli <tailor@u.washington.edu>
2014-05-31 13:26:32 -0700

    New manifest uses default remote name 'origin' instead of 'dev' or 'gh'

--- templates/move_template.sh
+++ templates/move_template.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pushd ~/pdgh/manifest && python ~/pdgh/python-practice/add-remove-xml-nodes/t.py -f ~/pdgh/manifest/default.xml --add "{{ project }}" && git commit -m "Add {{ project }} project" -a && git show --color
-git push && popd && git remote add gh git@github.com:taylormonacelli/{{ project }}.git
-git push --set-upstream gh master --force; git push gh --all
+git push && popd && git remote add origin git@github.com:taylormonacelli/{{ project }}.git
+git push --set-upstream origin master --force; git push origin --all
 python ~/pdgh/python-practice/add-remove-xml-nodes/t.py -f ~/pdgh/manifest/default.xml --remove "{{ project }}" && cd ~/pdgh/manifest && git diff --color && git commit -m "Move {{ project }} project to github" -a
 git push
 ssh dev
