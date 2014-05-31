2eeafba4cf932e2aa4703cbb97402769aaa23604
taylormonacelli <tailor@u.washington.edu>
2014-05-31 13:49:09 -0700

    Don't remove project from new manifest, remove it from old pdev/manifest

--- templates/move_template.sh
+++ templates/move_template.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-pushd ~/pdgh/manifest && python ~/pdev/python-practice/add-remove-xml-nodes/t.py -f ~/pdgh/manifest/default.xml --add "{{ project }}" && git commit -m "Add {{ project }} project" -a && git show --color
+pushd ~/pdgh/manifest && python ~/pdgh/python-practice/add-remove-xml-nodes/t.py -f ~/pdgh/manifest/default.xml --add "{{ project }}" && git commit -m "Add {{ project }} project" -a && git show --color
 git push && popd && git remote add gh git@github.com:taylormonacelli/{{ project }}.git
 git push --set-upstream gh master --force; git push gh --all
-python ~/pdev/python-practice/add-remove-xml-nodes/t.py -f ~/pdev/manifest/default.xml --remove "{{ project }}" && cd ~/pdev/manifest && git diff --color && git commit -m "Move {{ project }} project to github" -a
+python ~/pdgh/python-practice/add-remove-xml-nodes/t.py -f ~/pdev/manifest/default.xml --remove "{{ project }}" && cd ~/pdev/manifest && git diff --color && git commit -m "Move {{ project }} project to github" -a
 git push
 ssh dev
 mv /var/www/html/proj/{{ project }}.git /var/www/html/proj.movetogithub
